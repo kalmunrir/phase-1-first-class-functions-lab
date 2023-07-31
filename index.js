b145065d1b3cecc8ca9a2ad7c277f8fb71704345
Juan Ortiz-Acosta <jdoa2003@gmail.com>
2023-07-31 14:05:56 -0600

    Finished Lab

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Code your solution in this file!
+function returnFirstTwoDrivers(driverList){
+    return driverList.slice(0, 2);
+}
+
+function returnLastTwoDrivers(driverList){
+    return driverList.slice(-2);
+}
+
+const selectingDrivers = [returnFirstTwoDrivers, returnLastTwoDrivers];
+
+function createFareMultiplier(number) {
+    return (fare) => fare * number;
+}
+function fareDoubler(fare) {
+    return createFareMultiplier(2)(fare);
+}
+
+function fareTripler(fare) {
+    return createFareMultiplier(3)(fare);
+}
+
+function selectDifferentDrivers(driverList, driverFunction) {
+    return driverFunction(driverList);
+}
